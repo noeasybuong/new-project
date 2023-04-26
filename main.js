5eca82e26856b24be12cda4de20ab1a0d3f7fc23
noeasybuong <tangduchoang34@gmail.com>
2023-04-26 16:12:17 +0700

    commitgit

---
+++ main.js
@@ -0,0 +1,11 @@
+function myCreen() {
+   let x = document.getElementById('button')
+   let y = x.getAttribute('value');
+   document.getElementById('calculation').innerHTML= y;
+}
+// let x = document.get
+
+// function myResult() {
+//    let x = document.getElementById('')
+    
+// }
