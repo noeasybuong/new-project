26f31e828e05cdd9bb77cb3593ae3a18d0a45f86
noeasybuong <tangduchoang34@gmail.com>
2023-06-06 10:19:50 +0700

    commit

--- main.js
+++ main.js
@@ -1,13 +1,46 @@
-function myCreen() {
-let x = document.getElementsByName('button')[3];
-  let y = x.getAttribute('value');
-   document.getElementById('calculation').innerHTML= y;
-
+function clearCreen() {
+  document.getElementById("calculation").innerHTML = "";
+  document.getElementById("creen-result").innerHTML = "";
 }
 
-// let x = document.get
+//Creen
+function myCreen(value) {
+  document.getElementById("calculation").innerHTML += value;
+  document.getElementById("calculation").style.fontSize = "50px";
+}
 
-// function myResult() {
-//    let x = document.getElementById('')
-    
-// }
+//Result
+function myResult() {
+  let y = 0;
+  document.getElementById("creen-result").style.fontSize = "40px";
+  let x = document.getElementById("calculation").innerHTML;
+
+//Phép tính mũ 2
+if (x.includes("²")) {
+  let z = x.replace("²", " ");
+  y = Number(z);
+  return document.getElementById("creen-result").innerHTML = Math.pow(y,2);
+}
+
+
+
+
+  //Phép tính căn bậc 2
+  if (x.includes("√")) {
+    let z = x.replace("√", " ");
+    y = Number(z);
+    return document.getElementById("creen-result").innerHTML = Math.sqrt(y);
+  }
+
+
+
+
+  //Phép tính cơ bản
+  y = eval(x);
+
+  if (y / y.toFixed(2) != 1) {
+    document.getElementById("creen-result").innerHTML = y.toFixed(2);
+  } else {
+    document.getElementById("creen-result").innerHTML = y.toFixed(0);
+  }
+}
